d012dfd8c8d451dc1900e7937f466c72af3b1791
Priyam <priyamroy29@gmail.com>
2021-12-23 01:24:47 +0530

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function startApp() {
+	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+	app.useGlobalPipes(new ValidationPipe());
+	await app.listen(3000);
+	console.log(`Application is running on: ${await app.getUrl()}`);
+}
+startApp();
