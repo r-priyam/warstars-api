7650da22541013d3e68e51dc70cedf7db1ce0bea
Priyam <priyamroy29@gmail.com>
2021-12-23 10:39:26 +0530

    add paths in tsconfig

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
+import { AppModule } from '~/app.module';
 
 async function startApp() {
 	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
