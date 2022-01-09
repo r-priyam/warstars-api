adb3ec2659d3617a539dc29c5f0426ee08818b39
Priyam <priyamroy29@gmail.com>
2022-01-09 13:03:13 +0530

    fix: export `ClashService`

--- src/core/clash/clash.module.ts
+++ src/core/clash/clash.module.ts
@@ -2,5 +2,5 @@ import { Module } from '@nestjs/common';
 import { OgmaModule } from '@ogma/nestjs-module';
 import { ClashService } from './clash.service';
 
-@Module({ imports: [OgmaModule.forFeature(ClashService)], providers: [ClashService] })
+@Module({ imports: [OgmaModule.forFeature(ClashService)], providers: [ClashService], exports: [ClashService] })
 export class ClashModule {}
