66b4437f80d6c9f683c8a7daae820470aff0eb94
Priyam <priyamroy29@gmail.com>
2022-01-14 00:31:40 +0530

    chore: add more routes for discord according to use

--- src/routes/discord/discord.controller.ts
+++ src/routes/discord/discord.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpException, HttpStatus, Query, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Post, Query, Req, Res } from '@nestjs/common';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { AppConfig } from '~/core/config/env.getters';
 import { Authenticated } from '~/core/decorators/auth.decorator';
@@ -14,11 +14,26 @@ export class DiscordController {
 	}
 
 	@Get('callback')
-	async callback(@Query() query: { code: string }) {
+	async callback(@Query() query: { code: string }, @Res() response: FastifyReply) {
 		if (!query.code)
 			throw new HttpException('No code received. Please return back to homepage and try to authorize again.', HttpStatus.BAD_REQUEST);
 
-		return await this.discordService.handleCallback(query.code);
+		await this.discordService.handleCallback(query.code);
+		return response.status(302).redirect(this.config.discord.successRedirect);
+	}
+
+	@Get('user')
+	@Authenticated()
+	user(@Req() request: FastifyRequest) {
+		const user = request.session.user;
+		['id', 'email', 'accessToken', 'refreshToken'].forEach((e) => delete user[e]);
+		return user;
+	}
+
+	@Get('check')
+	@Authenticated()
+	checkLoggedIn() {
+		return;
 	}
 
 	@Get('guilds')
@@ -27,13 +42,13 @@ export class DiscordController {
 		return await this.discordService.userGuilds();
 	}
 
-	@Get('logout')
+	@Post('logout')
 	@Authenticated()
 	async logout(@Req() request: FastifyRequest, @Res() response: FastifyReply) {
 		await this.discordService.logOut();
 		request.destroySession((error) => {
 			if (error) throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
-			else response.status(302).redirect(this.config.logOutRedirectUrl);
+			else response.clearCookie('sessionId').status(302).redirect(this.config.logOutRedirectUrl);
 		});
 	}
 }
