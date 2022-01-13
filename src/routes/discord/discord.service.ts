f77b2846dfcdd7019b95f85a47fd358b1c2667b4
Priyam <priyamroy29@gmail.com>
2022-01-14 02:52:49 +0530

    refactor: remove all the session scopes and use param

--- src/routes/discord/discord.service.ts
+++ src/routes/discord/discord.service.ts
@@ -1,31 +1,29 @@
 import fetch from 'node-fetch';
 import * as CryptoJS from 'crypto-js';
 import { Repository } from 'typeorm';
-import { REQUEST } from '@nestjs/core';
 import { FastifyRequest } from 'fastify';
 import { InjectRepository } from '@nestjs/typeorm';
-import { HttpException, HttpStatus, Inject, Injectable, Scope } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 import { DatabaseSession, User } from '~/database';
 import { AppConfig } from '~/core/config/env.getters';
 import { ICreateUser, ICredentialsResponse, IDiscordUser, IDiscordUserGuild, IEncryptedTokens } from '~/utils/interfaces';
 
-@Injectable({ scope: Scope.REQUEST })
+@Injectable()
 export class DiscordService {
 	constructor(
 		private readonly config: AppConfig,
-		@Inject(REQUEST) private readonly request: FastifyRequest,
 		@InjectRepository(User) private userDB: Repository<User>,
 		@InjectRepository(DatabaseSession) private sessionDB: Repository<DatabaseSession>
 	) {}
 
-	async handleCallback(code: string) {
+	async handleCallback(request: FastifyRequest, code: string) {
 		try {
 			const oauthData = await this.exchangeToken('authorization_code', { code: code });
 			const userDiscordData: IDiscordUser = await this.getUserData(oauthData.access_token);
 			const tokens = this.encryptTokens(oauthData.access_token, oauthData.refresh_token);
 			const user = await this.createUser(this.userData(userDiscordData, tokens));
-			await this.createSession(user);
+			await this.createSession(request, user);
 		} catch (error) {
 			throw new HttpException({ status: HttpStatus.INTERNAL_SERVER_ERROR, error: error }, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
@@ -65,39 +63,40 @@ export class DiscordService {
 		return await this.userDB.save(user);
 	}
 
-	private async createSession(user: User) {
-		this.request.session.user = user;
+	private async createSession(request: FastifyRequest, user: User) {
+		request.session.user = user;
 		const session = this.sessionDB.create({
-			id: this.request.session.sessionId,
-			expiredAt: new Date(this.request.session.cookie.expires).getTime(),
+			id: request.session.sessionId,
+			expiredAt: new Date(request.session.cookie.expires).getTime(),
 			json: JSON.stringify(user)
 		});
 		return await this.sessionDB.save(session);
 	}
 
-	async handleTokenRefresh() {
+	async handleTokenRefresh(request: FastifyRequest) {
 		try {
-			const refreshToken = this.decryptToken(this.request.session.user.refreshToken).toString(CryptoJS.enc.Utf8);
+			const refreshToken = this.decryptToken(request.session.user.refreshToken).toString(CryptoJS.enc.Utf8);
+			console.log(refreshToken);
 			const refreshData = await this.exchangeToken('refresh_token', { refreshToken: refreshToken });
 			const userDiscordData: IDiscordUser = await this.getUserData(refreshData.access_token);
 			const tokens = this.encryptTokens(refreshData.access_token, refreshData.refresh_token);
 			const user = await this.createUser(this.userData(userDiscordData, tokens));
-			await this.createSession(user);
+			await this.createSession(request, user);
 		} catch (error) {
 			throw new HttpException({ status: HttpStatus.INTERNAL_SERVER_ERROR, error: error }, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
-	async userGuilds(): Promise<IDiscordUserGuild> {
-		const accessToken = this.decryptToken(this.request.session.user.accessToken).toString(CryptoJS.enc.Utf8);
+	async userGuilds(request: FastifyRequest): Promise<IDiscordUserGuild> {
+		const accessToken = this.decryptToken(request.session.user.accessToken).toString(CryptoJS.enc.Utf8);
 		const response = await fetch('https://discord.com/api/users/@me/guilds', { headers: { authorization: `Bearer ${accessToken}` } });
 		const data = await response.json();
 		if (!response.ok) throw new HttpException({ status: HttpStatus.BAD_REQUEST, error: data }, HttpStatus.BAD_REQUEST);
 		return data;
 	}
 
-	async logOut() {
-		const sessionId = this.request.session.sessionId;
+	async logOut(request: FastifyRequest) {
+		const sessionId = request.session.sessionId;
 		await this.sessionDB.delete({ id: sessionId });
 	}
 
