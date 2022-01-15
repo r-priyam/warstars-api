3cde5263db674f99047dcd1c6ebbfc8ea3eac479
Priyam <priyamroy29@gmail.com>
2022-01-16 00:14:32 +0530

    feat: use akiro with discordjs
    feat: add command for league badge
    chore: remove webpack

--- src/routes/league/register/register-event.listener.ts
+++ src/routes/league/register/register-event.listener.ts
@@ -1,65 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
-import { InjectRepository } from '@nestjs/typeorm';
-import { GuildMember, MessageActionRow, MessageButton, MessageEmbed, TextChannel } from 'discord.js';
-import { Repository } from 'typeorm';
+import { MessageActionRow, MessageButton, MessageEmbed, TextChannel } from 'discord.js';
 import { BotService } from '~/core/bot/bot.service';
-import { League, LeagueAdmin } from '~/database';
 import { IRegisterLeague } from '~/utils/interfaces';
 
 @Injectable()
 export class LeagueRegisterListener {
-	constructor(
-		private readonly discord: BotService,
-		@InjectRepository(League) private leagueDb: Repository<League>,
-		@InjectRepository(LeagueAdmin) private adminDb: Repository<LeagueAdmin>
-	) {
-		this.bot.on('interactionCreate', async (interaction) => {
-			if (!interaction.isButton()) return;
-			if (!interaction.customId.startsWith('APPROVE_') && !interaction.customId.startsWith('DECLINE_')) return;
-
-			const user = interaction.member as GuildMember;
-			if (/^APPROVE/i.test(interaction.customId)) {
-				const abbreviation = interaction.customId.replace('APPROVE_', '');
-
-				const leagueData = await this.leagueDb.query(
-					'UPDATE league SET is_verified = true WHERE abbreviation = $1 RETURNING league_id, name, abbreviation, discord_id',
-					[abbreviation]
-				);
-				await this.adminDb.query('INSERT INTO league_admin (discord_id, league_id, permissions) VALUES($1, $2, $3)', [
-					leagueData[0][0].discord_id,
-					leagueData[0][0].league_id,
-					1024
-				]);
-
-				const verifiedEmbed = new MessageEmbed()
-					.setColor('#2ECC71')
-					.setTitle('League Approved')
-					.setDescription(
-						`Successfully approved ${leagueData[0][0].name} application. Unique ID: ${leagueData[0][0].league_id}. Abbreviation: ${leagueData[0][0].abbreviation}`
-					)
-					.setFooter({ text: `Approved by - ${user.displayName}`, iconURL: user.displayAvatarURL({ dynamic: true }) })
-					.setTimestamp();
-				return await interaction.update({ embeds: [verifiedEmbed], components: [] });
-			}
-
-			const abbreviation = interaction.customId.replace('DECLINE_', '');
-			const leagueData = await this.leagueDb.query(
-				'DELETE FROM league WHERE abbreviation = $1 RETURNING league_id, name, abbreviation, discord_id',
-				[abbreviation]
-			);
-
-			const declineEmbed = new MessageEmbed()
-				.setColor('#E74C3C')
-				.setTitle('League Declined')
-				.setDescription(
-					`Successfully declined ${leagueData[0][0].name} application. Unique ID: ${leagueData[0][0].league_id}. Abbreviation: ${leagueData[0][0].abbreviation}`
-				)
-				.setFooter({ text: `Declined by - ${user.displayName}`, iconURL: user.displayAvatarURL({ dynamic: true }) })
-				.setTimestamp();
-			return await interaction.update({ embeds: [declineEmbed], components: [] });
-		});
-	}
+	constructor(private readonly discord: BotService) {}
 	private bot = this.discord.client;
 
 	@OnEvent('league.register')
