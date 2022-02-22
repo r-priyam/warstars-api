c92be893ff60725bedee0466bfdb042de3663105
Priyam <priyamroy29@gmail.com>
2022-02-23 00:35:09 +0530

    chore: make code not look bad?

--- src/main.ts
+++ src/main.ts
@@ -22,19 +22,17 @@ async function main() {
     });
 
     const config = app.get(AppConfig);
-    const sessionStore = getRepository(DatabaseSession);
-
-    const logger = app.get<OgmaService>(OgmaService);
-    app.useLogger(logger);
-
-    const coc = app.get<ClashService>(ClashService);
-    await coc.init();
-
     const discord = app.get<BotService>(BotService);
+    const logger = app.get<OgmaService>(OgmaService);
+    const sessionStore = getRepository(DatabaseSession);
+    const clashClient = app.get<ClashService>(ClashService);
+
+    app.useLogger(logger);
+    app.enableCors({ origin: config.corsOrigins, credentials: true });
     await discord.init();
+    await clashClient.init();
 
     await app.register(fastifyHelmet);
-    app.enableCors({ origin: config.corsOrigins, credentials: true });
     await app.register(fastifyCookie, { secret: [config.sessionCookieSecret, config.cookieSignSecret] });
     await app.register(fastifySession, {
         cookie: {
