71be9900d214cb6d2b3ffabddede60a1f031df66
r-priyam <priyamroy29@gmail.com>
2022-02-27 22:29:31 +0530

    chore: add script for colors and code format

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+. ./colors.sh
 
 CONTAINER_NAME='warstars-database'
 DUMP_FILE="dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql"
@@ -8,7 +9,7 @@ DUMP_FILE="dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql"
 cd ../
 
 if [ "$( docker container inspect -f '{{.State.Running}}' $CONTAINER_NAME )" == "true" ]; then
-  echo "$(tput setaf 4)Starting database dump"
+  echo "${BLUE}Starting database dump"
 
   if [ -d 'database-dump' ]; then
     cd database-dump
@@ -18,10 +19,10 @@ if [ "$( docker container inspect -f '{{.State.Running}}' $CONTAINER_NAME )" == 
 
   docker exec -t $CONTAINER_NAME pg_dumpall -c -U warstars > $DUMP_FILE
   DUMP_SIZE=$(ls -lh $DUMP_FILE | awk '{print  $5}')
-  echo "$(tput setaf 3)Dump size - $DUMP_SIZE"
-  echo "$(tput setaf 2)Database dump completed"
+  echo "${YELLOW}Dump size - $DUMP_SIZE"
+  echo "${GREEN}Database dump completed"
 
 else
-  echo "$(tput setaf 1) $CONTAINER_NAME isn't running"
+  echo "${RED}$CONTAINER_NAME isn't running"
   exit 1
 fi
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 set -e
+. ./colors.sh
 
 cd ../
 if [[ $1 == 'dev' ]];
 then
-  echo "$(tput setaf 4)Starting docker for development"
+  echo "${BLUE}Starting docker for development"
   docker-compose -f docker-compose.dev.yml up
 
 elif [[ $1 == 'prod' ]];
 then
-  echo "$(tput setaf 2)Starting docker for production"
+  echo "${GREEN}Starting docker for production"
   docker-compose up -d
 
 else
-  echo "$(tput setaf 1)Expected 'dev' or 'prod' as an argument"
+  echo "${RED}Expected 'dev' or 'prod' as an argument"
   exit 1
 fi
