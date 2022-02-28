eb66c680a7b2b5e8a99db943ae07f1c94d7d4b28
r-priyam <priyamroy29@gmail.com>
2022-02-28 14:44:12 +0530

    feat: add script for database restore

---
+++ scripts/restore.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+. ./colors.sh
+
+cd ../
+
+[ ! -d "database-dump" ] && { echo "{RED}Database dump directory not found"; exit 1; }
+
+cd database-dump
+
+echo "${BLUE}${BOLD}Enter dump file name to restore -${RESET}"
+read FILE_NAME
+
+if [ -f "$FILE_NAME" ]; then
+    echo "${BLUE}Starting databse dump restore"
+    cat $FILE_NAME | docker exec -i warstars-database psql -U warstars -d warstars
+    echo "${GREEN}Restore completed"
+
+else
+  echo "${BOLD}${BOLD}Database dump file not found!"
+  exit 1
+fi
