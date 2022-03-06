3dd85d1d96aa7f833e4fe126d2cba367da77a11f
r-priyam <priyamroy29@gmail.com>
2022-03-06 10:45:14 +0530

    chore: modify cache logging

--- src/core/redis/cache.service.ts
+++ src/core/redis/cache.service.ts
@@ -5,6 +5,7 @@ import { Store } from 'clashofclans.js';
 
 import { RedisService } from '~/core/redis/redis.service';
 
+// This service is just made for clashofclans caching. Maybe merge with redis service itself in future?
 @Injectable()
 export class CacheService implements Omit<Store, 'delete' | 'clear'> {
     public constructor(@OgmaLogger(CacheService) private readonly logger: OgmaService, private readonly redisService: RedisService) {}
@@ -15,9 +16,11 @@ export class CacheService implements Omit<Store, 'delete' | 'clear'> {
         await this.redis.set(key, JSON.stringify(value), { PX: ttl });
 
         this.logger.debug(
-            `${style.green.bold.apply('Cached Data')} | ${style.yellow.bold.apply('KEY:')} ${key} | ${style.red.bold.apply(
-                'EXPIRY:'
-            )} ${ttl} | ${style.blue.bold.apply('VALUE:')} ${JSON.stringify(value).substring(0, 40)} ...`
+            `${style.green.bold.apply('Cached Data')} | 
+            ${style.yellow.bold.apply('KEY:')} ${key} | 
+            ${style.red.bold.apply('EXPIRY:')} ${ttl} | 
+            ${style.blue.bold.apply('VALUE:')} 
+            ${JSON.stringify(value).substring(0, 40)} ...`.replace(/\n|\r|\s+/g, ' ')
         );
         return true;
     }
@@ -27,9 +30,9 @@ export class CacheService implements Omit<Store, 'delete' | 'clear'> {
         if (data === null) return null;
 
         this.logger.debug(
-            `${style.magenta.bold.apply('Returning Cached Data')} | ${style.yellow.bold.apply('KEY:')} ${key} | ${style.blue.bold.apply(
-                'DATA:'
-            )} ${data.substring(0, 40)} ...`
+            `${style.color(214).bold.apply('Returning Cached Data')} | 
+            ${style.yellow.bold.apply('KEY:')} ${key} | 
+            ${style.blue.bold.apply('DATA:')} ${data.substring(0, 40)} ...`.replace(/\n|\r|\s+/g, ' ')
         );
         return JSON.parse(data);
     }
