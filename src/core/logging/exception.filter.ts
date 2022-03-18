49a1c4c11ad0a5a19825cf8f2d6df04ec0cae7dc
r-priyam <priyamroy29@gmail.com>
2022-03-18 14:37:15 +0530

    feat: add discord webhook notifier

--- src/core/logging/exception.filter.ts
+++ src/core/logging/exception.filter.ts
@@ -1,18 +1,31 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
 import { HttpAdapterHost } from '@nestjs/core';
 import { OgmaService } from '@ogma/nestjs-module';
+import { MessageEmbed, WebhookClient } from 'discord.js';
 
 @Catch()
 export class ExceptionsFilter implements ExceptionFilter {
     constructor(private readonly httpAdapterHost: HttpAdapterHost, private readonly logger: OgmaService) {}
 
-    catch(exception: any, host: ArgumentsHost): void {
+    async catch(exception: any, host: ArgumentsHost) {
+        const webhook = new WebhookClient({
+            id: process.env.WEBHOOK_ID,
+            token: process.env.WEBHOOK_TOKEN
+        });
+
         const { httpAdapter } = this.httpAdapterHost;
         const ctx = host.switchToHttp();
         const httpStatus = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
 
         if (httpStatus === 500) {
             this.logger.printError(exception);
+            const embed = new MessageEmbed()
+                .setTitle('Error')
+                .setDescription(`\`\`\`js\n${String(exception).substring(0, 4000)}\`\`\``)
+                .addFields({ name: 'Path', value: `\`${httpAdapter.getRequestUrl(ctx.getRequest())}\`` })
+                .setColor('#ff0000')
+                .setTimestamp();
+            process.env.ENV === 'production' && (await webhook.send({ content: '<@554882868091027456>, ERROR', embeds: [embed] }));
         }
 
         const responseBody = {
